dcc42387f584583c229b0fd0b69fec630e201298
Виталий Котенко <caunt.official@gmail.com>
2020-06-15 12:27:04 +0300

    Increase on RangeVector

---
+++ Source/Functions/Range/IncreaseFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace PromQL.Functions.Range
+{
+    public class IncreaseFunction : BaseFunction<IncreaseFunction>
+    {
+        private IncreaseFunction() : base("increase")
+        {
+        }
+
+        public static IncreaseFunction Create()
+        {
+            return new IncreaseFunction();
+        }
+    }
+}
--- Source/RangeVector.cs
+++ Source/RangeVector.cs
@@ -123,6 +123,17 @@ namespace PromQL
         }
 
         /// <summary>
+        /// Calculates the increase in the time series in the range vector. Breaks in monotonicity (such as counter resets due to target restarts) are automatically adjusted for. The increase is extrapolated to cover the full time range as specified in the range vector selector, so that it is possible to get a non-integer result even if a counter increases only by integer increments.
+        /// Increase should only be used with counters. It is syntactic sugar for rate(v) multiplied by the number of seconds under the specified time range window, and should be used primarily for human readability. Use rate in recording rules so that increases are tracked consistently on a per-second basis.
+        /// </summary>
+        /// <returns></returns>
+        public InstantVector Increase()
+        {
+            actions.Add(IncreaseFunction.Create());
+            return InstantVector.WithName(this);
+        }
+
+        /// <summary>
         /// Calculates the difference between the last two samples in the range vector v, returning an instant vector with the given deltas and equivalent labels.
         /// InstantDelta should only be used with gauges.
         /// </summary>
